986e16fee3c87fbdf26cfe0f73ef6d54c9bfc511
DxDanDxD <138779463+DxDanDxD@users.noreply.github.com>
2023-07-07 15:38:46 +0300

    upd1
    upd1

--- bottt000001.py
+++ bottt000001.py
@@ -1,4 +1,4 @@
-from config import TOKEN
+"""from config import TOKEN
 
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -6,13 +6,24 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'],state='*')
+async def start_handler(message: types.Message,state:FSMContext):
+    await message.answer("Добро пожаловать в тестовый бот. Как тебя зовут?")
+    await state.set_state("name")
+
+
+@dp.message_handler(state="age")
+async def age_handler(message: types.Message, state: FSMContext):
+    await message.answer("nf aeyrwbz ybrjulf yt dspjdtncz")
 
 
 @dp.message_handler(commands=['start'])
-async def start_handler(message: types.Message):
-    """Ловец - обработчик сообщений"""
-    await message.answer("Добро пожаловать в тестовый бот.")
+async def name_handler(message: types.Message,state:FSMContext):
+    await message.answer("Поздравляю! У тебя стейт name")
+    name=message.text\
 
 
 @dp.message_handler(content_types=['text'])
@@ -20,9 +31,14 @@ async def start_handler(message: types.Message):
     await message.answer(message.text)
 
 
-"""@dp.message_handler(content_types=['text'])
+@dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-    await bot.send_message(message.text)"""
+    await message.answer("Добро пожаловать в тестовый бот.")
 
 
-executor.start_polling(dp, skip_updates=True)
+@dp.message_handler(content_types=['text'])
+async def start_handler(message: types.Message):
+    await bot.send_message(message.text)
+
+
+executor.start_polling(dp, skip_updates=True)"""
